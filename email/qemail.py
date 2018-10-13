107998a019400181cd2d1748f7f7b264a62a667c
e <qxy@jsyzqxy.cn>
2018-10-13 22:46:08 +0800

    The latest version of send_mail module

--- email/qemail.py
+++ email/qemail.py
@@ -2,12 +2,15 @@
 # 封装发送邮件的类
 # 添加密码参数 若调用函数前未设置密码则需要进行登录操作
 # 将变量输入类型都统一字符型输入(适当list型也允许) 函数中进行处理
-# 返回邮件发送结果(-3:输入参数出错 -2:添加附件出错 -1:发送邮件失败(网络、邮箱&密码不匹配等原因) 0:发送成功)(-3、-2、-1均不会发送出去邮件)
+# 返回邮件发送结果(-3:输入参数格式出错 -2:添加附件出错 -1:发送邮件失败(网络、邮箱&密码不匹配等原因) 0:发送成功)(-3、-2、-1均不会发送出去邮件)
+# regex 判断邮箱格式是否正确
+# 将处理过程写成一个 preprocess 函数 降低耦合性
 
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import base64
+import re
 
 class SendMail(object):
     '''This is a class to send an email to someone.
@@ -29,38 +32,64 @@ You will get four different status codes after calling this function:
     def __init__(self, username_recv, mailtitle, mailcontent, file = [], 
                 username_send = "robot@jsyzqxy.cn", password_send = "",
                  mailserver = "smtp.exmail.qq.com", port = 465):
-        # 对于数据类型进行字符型统一
-        # 判断单个还是多个收件人
-        if not isinstance(username_recv, list):
-            self.username_recv = [str(username_recv)]
-        else:
-            for i in range(len(username_recv)):
-                username_recv[i] = str(username_recv[i])
-            self.username_recv = username_recv
-        self.mailtitle = str(mailtitle)
-        self.mailcontent = str(mailcontent)
-        # 判断单个还是多个附件
-        if not isinstance(file, list):
-            self.file = [str(file)]
-        else:
-            for i in range(len(file)):
-                file[i] = str(file[i])
-            self.file = file
-        self.username_send = str(username_send)
-        self.password_send = str(password_send)
-        self.mailserver = str(mailserver)
-        self.port = str(port)
+        self.username_recv = username_recv
+        self.mailtitle = mailtitle
+        self.mailcontent = mailcontent
+        self.file = file
+        self.username_send = username_send
+        self.password_send = password_send
+        self.mailserver = mailserver
+        self.port = port
 
-    def send_mail(self):
+    def preprocess(self):
+        # 对于数据类型进行统一预处理
+        # 对于变量类型、邮箱格式是否合法进行验证以及处理
+        # 将 file、 username_recv 转换成 list 型
+        charge_mail = re.compile(r"\w+@\w+\.[a-zA-Z]+") # 邮箱匹配 regex
+        ## username_recv
+        if not isinstance(self.username_recv, list):
+            self.username_recv = str(self.username_recv)
+            if charge_mail.match(self.username_recv) == None:
+                return False
+            self.username_recv = [self.username_recv]
+        else:
+            for i in range(len(self.username_recv)):
+                self.username_recv[i] = str(self.username_recv[i])
+                if charge_mail.match(self.username_recv[i]) == None:
+                    return False
+        ## mailtitle
+        self.mailtitle = str(self.mailtitle)
+        ## mailcontent
+        self.mailcontent = str(self.mailcontent)
+        ## file
+        # 判断单个还是多个附件
+        if not isinstance(self.file, list):
+            self.file = [str(self.file)]
+        else:
+            for i in range(len(self.file)):
+                self.file[i] = str(self.file[i])
+        ## username_send
+        self.username_send = str(self.username_send)
+        if charge_mail.match(self.username_send) == None:
+            return False
+        ## password_send
+        self.password_send = str(self.password_send)
         # 如果没有提前输入密码 执行登录
         if self.password_send == "":
             print("You are trying to use account %s to send an email."%self.username_send)
             self.password_send = input("Please enter the password:")
-        # 对于port是否合法进行验证以及处理
-        # 可在此处使用 regex 进行名称格式验证
+        ## mailserver
+        self.mailserver = str(self.mailserver)
+        ## port
         try:
             self.port = int(self.port)
         except Exception:
+            return False
+        return True
+
+    def send_mail(self):
+        # 进行预处理
+        if not self.preprocess():
             return -3
         # 建立邮件对象
         mail = MIMEMultipart()
@@ -68,7 +97,7 @@ You will get four different status codes after calling this function:
         mail["Subject"] = self.mailtitle
         mail["From"] = self.username_send
         mail["To"] = ",".join(self.username_recv)
-        # 处理附件
+        # 添加附件
         for i in range(0, len(self.file)):
             try:
                 att = MIMEText(open(self.file[i], "rb").read(), "base64", "utf-8")
@@ -82,7 +111,7 @@ You will get four different status codes after calling this function:
         # 发送邮件
         try:
             self.smtp = smtplib.SMTP_SSL(self.mailserver, port = self.port)
-            self.smtp.login(self.username_send, self.password_send) #登录密码末尾空格会被忽视
+            self.smtp.login(self.username_send, self.password_send) # 登录密码末尾空格会被忽视
             self.smtp.sendmail(self.username_send, self.username_recv, mail.as_string())
             self.smtp.quit()
         except Exception as e:
