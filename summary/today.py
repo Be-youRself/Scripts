ada3992b2d504e06c517cdcd3a9a5a6d19a93845
e <qxy@jsyzqxy.cn>
2018-10-24 23:30:56 +0800

    Delete

--- summary/today.py
+++
@@ -1,8 +0,0 @@
-from PyQt5 import QtWidgets
-from ui_today import Ui_Today
-import sys
-
-class Ui_Today_sub(QtWidgets.QWidget, Ui_Today): #多态继承
-    def __init__(self, parent = None):
-        super(Ui_Today_sub, self).__init__(parent = parent)
-        self.setupUi(self)       
