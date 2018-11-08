892feaa03acf7db16affad9f0eda48ebea6e6fbf
e <qxy@jsyzqxy.cn>
2018-11-08 23:58:51 +0800

    Yd_Translation

--- Yd_Translation/Yd_Translation.py
+++ Yd_Translation/Yd_Translation.py
@@ -1,37 +1,53 @@
-#/usr/bin/env python
-#coding=utf8
- 
-import httplib
-import md5
-import urllib
+# Filename: Yd_Translation.py
+# Coding: utf8
+#
+
+import http.client # python 2 对应 httplib
+from hashlib import md5
+import urllib.request # python 2 对应 urllib
 import random
 
-appKey = '你的appkey'
-secretKey = '您的应用密钥'
 
- 
-httpClient = None
-myurl = '/api'
-q = 'good'
-fromLang = 'EN'
-toLang = 'zh-CHS'
-salt = random.randint(1, 65536)
+def en2chs(text):
+    '''
+    :param : text (which need to be translated)
+    :return: json string as a result
+    '''
+    appKey = "应用ID"
+    secretKey = "应用密钥"
 
-sign = appKey+q+str(salt)+secretKey
-m1 = md5.new()
-m1.update(sign)
-sign = m1.hexdigest()
-myurl = myurl+'?appKey='+appKey+'&q='+urllib.quote(q)+'&from='+fromLang+'&to='+toLang+'&salt='+str(salt)+'&sign='+sign
- 
-try:
-    httpClient = httplib.HTTPConnection('openapi.youdao.com')
-    httpClient.request('GET', myurl)
- 
-    #response是HTTPResponse对象
-    response = httpClient.getresponse()
-    print response.read()
-except Exception, e:
-    print e
-finally:
-    if httpClient:
-        httpClient.close()
+    httpClient = None
+    myurl = '/api'
+    q = text
+    fromLang = 'EN'
+    toLang = 'zh-CHS'
+    salt = random.randint(1, 65536)
+
+    sign = appKey + q + str(salt) + secretKey
+    m1 = md5()
+    m1.update(sign.encode("utf8")) # python 3 中必须要编码后进行 update
+    sign = m1.hexdigest()
+    myurl = myurl + '?appKey=' + appKey + '&q=' + urllib.request.quote(
+        q) + '&from=' + fromLang + '&to=' + toLang + '&salt=' + str(salt) + '&sign=' + sign
+
+    try:
+        httpClient = http.client.HTTPConnection('openapi.youdao.com')
+        httpClient.request('GET', myurl)
+
+        # response是HTTPResponse对象
+        response = httpClient.getresponse()
+
+        #print("type:" + type(response.read()))
+        print(response.read())
+        #result = str(response.read())
+        #print("type(str):" + type(result))
+        #print(result)
+
+    except Exception as e:
+        print(e)
+    finally:
+        if httpClient:
+            httpClient.close()
+
+if __name__ == "__main__":
+    en2chs("good")
