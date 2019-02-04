a94d8bcc939169184c08a164e6d865fb40b69460
Be-youRself <qxy@jsyzqxy.cn>
2019-02-04 20:30:49 +0800

    New improvement
    New improvement.

--- wxpy/群发祝福/send_all.py
+++ wxpy/群发祝福/send_all.py
@@ -19,8 +19,15 @@ while True:
         break
 file_r.close()
 
+count = 0 # 用于统计人数
 # 发送祝福
-for i in friends[0:]:
-	i.send('亲爱的{0}：\n        {1}'.format(i.name, message)) # 四个空格等于一个汉字
+for i in friends[1:]: # 第一个是自己 所以从下标1开始 不除去自己会导致异常
+	try:
+		i.send('亲爱的{0}：\n        {1}'.format(i.name, message)) # 四个空格等于一个汉字
+	except Exception: # 防止出现异常中断脚本 尤其图片发送会导致异常！
+		print(i.name)
+		continue
+	count = count + 1
+	time.sleep(2) # 微信有消息发送速度限制
 
-print("成功发送祝福给了{0}个好友！".format(len(friends)))
+print("成功发送祝福给了{0}个好友！".format(count))
